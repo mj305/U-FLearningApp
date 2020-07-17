6930bf629901beff2d8e1f541344f811fd15f350
mj305 <maria.j.beckles@gmail.com>
2020-07-17 13:40:51 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
+import PrimarySearchAppBar from './Components/NavBar/NavBarComponent';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <PrimarySearchAppBar />
       <header className="App-header">
        <h1>Hackathon Educational App</h1>
       </header>
